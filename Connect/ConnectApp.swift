6689534d58c9e6801421ec41ffbf744164071c32
Chris <cullm001@ucr.edu>
2023-04-29 10:05:56 -0700

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by Chris Ullman on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
